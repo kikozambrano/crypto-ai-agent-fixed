2eac920803d61cd37994c54ab5521fb3001f4cb9
Francisco Zambrano <kikozambrano@gmail.com>
2025-04-07 13:52:54 -0600

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import ta
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 # === Settings ===
